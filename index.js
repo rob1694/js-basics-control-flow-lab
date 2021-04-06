b76806db711fb771ab2c75fd5bf5aac0a2d5bb3e
rob1694 <robertb1694@gmail.com>
2021-04-06 12:18:56 -0400

    complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if (num <= 400) {
+    return 'This one is on me!';
+  }
+    else if (num >= 2000 && num <= 2499){
+      return 'I will gladly take your thirty bucks.';
+    }
+    else if (num > 2500){
+      return 'No can do.';
+    }
+  }
+  
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC'){
+    return "Ok, sounds good."
+  } else {
+  return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === 'generous') {
+return "Thank you so much.";
+  } else if (tip ==='not as generous') {
+return "Thank you.";
+  } else {
+    return "Bye."; 
+  }
 }
